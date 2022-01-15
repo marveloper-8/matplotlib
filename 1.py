3d13372df5ea0b15a65fd5864410f0bcea5a2ff4
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 16:49:05 +0100

    tab20

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='tab20b')
+plt.scatter(a, b, c=c, cmap='tab20')
 
 plt.colorbar()
 plt.show()
