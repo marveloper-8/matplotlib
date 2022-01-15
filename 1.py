257ec96223547c7c622d862cf8fdde0aa5f4c034
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 16:54:07 +0100

    summer

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='tab10')
+plt.scatter(a, b, c=c, cmap='summer')
 
 plt.colorbar()
 plt.show()
