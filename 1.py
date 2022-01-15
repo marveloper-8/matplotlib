8a98e6afd6081ed78c3bca68d4f152cb474cdbba
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 10:31:04 +0100

    pyplot

---
+++ 1.py
@@ -0,0 +1,11 @@
+# import matplotlib
+# print(matplotlib.__version__)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = np.array([0, 6])
+b = np.array([0, 250])
+
+plt.plot(a, b)
+plt.show
