7f434ca3c2a5b618a42d5b7fb70a8bbd678c9bef
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 17:28:18 +0100

    YlGn

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='Wistia_r')
+plt.scatter(a, b, c=c, cmap='YlGn')
 
 plt.colorbar()
 plt.show()
