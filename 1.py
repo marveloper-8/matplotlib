57ccb16068b3f84556588b187bd5d212d7f606f1
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 17:48:21 +0100

    bwr reversed

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='bwr')
+plt.scatter(a, b, c=c, cmap='bwr_r')
 
 plt.colorbar()
 plt.show()
