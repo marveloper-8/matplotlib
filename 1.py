90847ba649da7fe346338ac69fd1a9a1d2fd994f
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 17:29:22 +0100

    YlGnBu

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='YlGnBu_r')
+plt.scatter(a, b, c=c, cmap='YlGnBu')
 
 plt.colorbar()
 plt.show()
