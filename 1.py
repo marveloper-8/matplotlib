6d7eb981fc745485e6edd8f9117b0a681a23ee60
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 11:58:58 +0100

    specify x and y values for 2 lines

--- 1.py
+++ 1.py
@@ -28,9 +28,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = np.array([3, 8, 1, 10])
-b = np.array([6, 2, 7, 11])
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
 
-plt.plot(a)
-plt.plot(b)
+plt.plot(x1, y1, x2, y2)
 plt.show()
