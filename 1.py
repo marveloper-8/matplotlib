2920fdfde8f3fb4d86719a545991acc158831136
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 10:44:24 +0100

    plot multiple lines

--- 1.py
+++ 1.py
@@ -13,8 +13,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = np.array([1, 8])
-b = np.array([3, 10])
+a = np.array([1, 2, 6, 8])
+b = np.array([3, 8, 1, 10])
 
-plt.plot(a, b, 'o')
+# plt.plot(a, b, 'o')
+plt.plot(a, b)
 plt.show()
