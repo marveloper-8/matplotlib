584924965a1df21087a936acd6d96ef31ecc9dea
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 16:45:38 +0100

    twilight reverse

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='twilight')
+plt.scatter(a, b, c=c, cmap='twilight_r')
 
 plt.colorbar()
 plt.show()
