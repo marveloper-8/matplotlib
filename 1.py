8d1669647d66721a0b632acefc336a497051dcc6
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 12:28:00 +0100

    display multiple plots

--- 1.py
+++ 1.py
@@ -36,20 +36,37 @@
 # plt.plot(x1, y1, x2, y2)
 # plt.show()
 
-import numpy as np
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# a = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# b = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# c = {'family': 'serif', 'color': 'blue', 'size': 20}
+# d = {'family': 'serif', 'color': 'darkred', 'size': 15}
+
+
+# plt.title("Sports Watch Data", fontdict=c, loc='left')
+# plt.xlabel("Average Pulse", fontdict=d)
+# plt.ylabel("Calorie Burnage", fontdict=d)
+
+# plt.plot(a, b)
+# plt.grid(color='green', linestyle='--', linewidth=0.5)
+# plt.show()
+
 import matplotlib.pyplot as plt
+import numpy as np
 
-a = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-b = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+a = np.array([0, 1, 2, 3])
+b = np.array([3, 8, 1, 10])
 
-c = {'family': 'serif', 'color': 'blue', 'size': 20}
-d = {'family': 'serif', 'color': 'darkred', 'size': 15}
-
-
-plt.title("Sports Watch Data", fontdict=c, loc='left')
-plt.xlabel("Average Pulse", fontdict=d)
-plt.ylabel("Calorie Burnage", fontdict=d)
-
+plt.subplot(1, 2, 1)
 plt.plot(a, b)
-plt.grid(color='green', linestyle='--', linewidth=0.5)
+
+a = np.array([0, 1, 2, 3])
+b = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(a, b)
+
 plt.show()
