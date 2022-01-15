8ef8ed7faf882223e23ced1637d77a345786e0eb
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 16:44:02 +0100

    viridis

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='viridis_r')
+plt.scatter(a, b, c=c, cmap='viridis')
 
 plt.colorbar()
 plt.show()
