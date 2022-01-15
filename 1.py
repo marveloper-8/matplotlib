cf4d0ec77b68bbfffbcdd7f171aaa585d8274bd8
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 17:35:10 +0100

    gnuplot

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='gnuplot2_r')
+plt.scatter(a, b, c=c, cmap='gnuplot')
 
 plt.colorbar()
 plt.show()
