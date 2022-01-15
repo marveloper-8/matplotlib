37eb7a485aba41e9b19532fe5bb75562de8fa753
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 16:55:18 +0100

    spring reverse

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='spring')
+plt.scatter(a, b, c=c, cmap='spring_r')
 
 plt.colorbar()
 plt.show()
