a4fb766816b6665e02e8314e74419be78ac65e59
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 16:42:34 +0100

    winter reverse

--- 1.py
+++ 1.py
@@ -80,7 +80,7 @@ import numpy as np
 a = np.random.randint(100, size=(100))
 b = np.random.randint(100, size=(100))
 c = np.random.randint(100, size=(100))
-plt.scatter(a, b, c=c, cmap='winter')
+plt.scatter(a, b, c=c, cmap='winter_r')
 
 plt.colorbar()
 plt.show()
