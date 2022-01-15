aad74827502a14e1a8fde4efe09924a867880cae
Joshua Equere <marveloper.8@gmail.com>
2022-01-15 10:41:59 +0100

    plotting x and y points

--- 1.py
+++ 1.py
@@ -1,11 +1,20 @@
 # import matplotlib
 # print(matplotlib.__version__)
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# a = np.array([0, 6])
+# b = np.array([0, 250])
+
+# plt.plot(a, b)
+# plt.show
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = np.array([0, 6])
-b = np.array([0, 250])
+a = np.array([1, 8])
+b = np.array([3, 10])
 
 plt.plot(a, b)
-plt.show
+plt.show()
